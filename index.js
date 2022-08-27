c5ac84c84db3c71eb0648b34b8495dc66a5600d6
John <johnudoumoh63@gmail.com>
2022-08-27 16:18:28 +0100

    n

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
-
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
