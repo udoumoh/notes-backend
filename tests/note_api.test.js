25e258eb0b50d63d62e6324d38ddf51a044352a6
John <johnudoumoh63@gmail.com>
2023-04-14 00:04:25 +0100

    .

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -31,21 +31,41 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-}, 100000)
+})
 
 
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
-
   expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
-
   const contents = response.body.map(r => r.content)
   expect(contents).toContain(
     'Browser can execute only Javascript'
+  )
+})
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  expect(response.body).toHaveLength(initialNotes.length + 1)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
   )
 })
 afterAll(() => {
