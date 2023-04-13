07067c43db0bedef1ec7389ee172a2dc92bfbbaa
John <johnudoumoh63@gmail.com>
2023-04-14 00:16:14 +0100

    LN

--- controllers/notes.js
+++ controllers/notes.js
@@ -29,7 +29,7 @@ notesRouter.post('/', (request, response, next) => {
 
   note.save()
     .then(savedNote => {
-      response.json(savedNote)
+      response.status(201).json(savedNote)
     })
     .catch(error => next(error))
 })
